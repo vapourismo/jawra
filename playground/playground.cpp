36a916e607ac5cde32388488cb15c04ecdd6e619
Ole <ole@vprsm.de>
2015-11-24 01:18:12 +0100

    Set up basis for ValueWrapping

--- playground/playground.cpp
+++ playground/playground.cpp
@@ -1,5 +1,19 @@
 #include <jawra.hpp>
 
+template <typename T>
+struct ValueWrapper {
+	static
+	bool check(v8::Isolate* isolate, v8::Local<v8::Value>);
+
+	static
+	T unpack(v8::Isolate* isolate, v8::Local<v8::Value>);
+
+	static
+	v8::Local<v8::Value> pack(v8::Isolate* isolate, T);
+
+	static_assert(sizeof(T) == -1, "Invalid specialization of ValueWrapper");
+};
+
 static
 void playground_init(v8::Local<v8::Object> module) {
 
